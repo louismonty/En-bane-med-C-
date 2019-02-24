41a920d46dc0feb61f4cb8269f5b49fa2760c3e1
louismonty <louis.mail.dk@gmail.com>
2019-02-24 22:56:32 +0100

    projekct2

---
+++ map.cpp
@@ -0,0 +1,68 @@
+#include "Map.h"
+
+int Line_to_print;
+
+void draw_map(){
+
+char character_line1[] = "+                                                                              +\n";
+char character_line2[] = "+                                                                              +\n";
+char character_line3[] = "+                                                                              +\n";
+char character_line4[] = "+                                                                              +\n";
+char character_line5[] = "+                                                                              +\n";
+char character_line6[] = "+                                                                              +\n";
+char character_line7[] = "+                                                                              +\n";
+char character_line8[] = "+                                                                              +\n";
+char character_line9[] = "+                                                                              +\n";
+char character_line10[] = "+                                                                              +\n";
+char character_line11[] = "+                                                                              +\n";
+char character_line12[] = "+                                                                              +\n";
+char character_line13[] = "+                                                                              +\n";
+char character_line14[] = "+                                                                              +\n";
+char character_line15[] = "+                                                                              +\n";
+char character_line16[] = "+                                                                              +\n";
+char character_line17[] = "+                                                                              +\n";
+char character_line18[] = "+                                                                              +\n";
+char character_line19[] = "+                                                                              +\n";
+char character_line20[] = "+                                                                              +\n";
+char character_line21[] = "+                                                                              +\n";
+char character_line22[] = "+                                                                              +\n";
+
+std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+
+char character_line1[4] = "x";
+
+std::cout << character_line1[0];
+
+//int pos_y = 50;
+
+//char charater_line1[] = "x";
+
+std::cout << character_line1;
+std::cout << character_line2;
+std::cout << character_line3;
+std::cout << character_line4;
+std::cout << character_line5;
+std::cout << character_line6;
+std::cout << character_line7;
+std::cout << character_line8;
+std::cout << character_line9;
+std::cout << character_line10;
+std::cout << character_line11;
+std::cout << character_line12;
+std::cout << character_line13;
+std::cout << character_line14;
+std::cout << character_line15;
+std::cout << character_line16;
+std::cout << character_line17;
+std::cout << character_line18;
+std::cout << character_line19;
+std::cout << character_line20;
+std::cout << character_line21;
+std::cout << character_line22;
+
+std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+
+int wait;
+
+std::cin >> wait;
+};
